6ed7aabce0a21db05b78201e4d0f4ca20ef78a0a
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-04-09 15:10:32 +0300

    Added controller (started)

---
+++ src/main/java/org/yarek/fasttestapp/routing/Controller.java
@@ -0,0 +1,20 @@
+package org.yarek.fasttestapp.routing;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/*")
+public class Controller extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
