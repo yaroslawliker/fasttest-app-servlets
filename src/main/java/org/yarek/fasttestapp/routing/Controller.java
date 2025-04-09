6e8918d7cf42a05591be44a2e04b9c1a7826a74e
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-04-09 15:21:50 +0300

    Removed unnececary comints in Controller

--- src/main/java/org/yarek/fasttestapp/routing/Controller.java
+++ src/main/java/org/yarek/fasttestapp/routing/Controller.java
@@ -1,8 +1,6 @@
 package org.yarek.fasttestapp.routing;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
