de83efd88d818f2a1d0606470e7381c4bb36c077
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-04-09 15:37:07 +0300

    Added handler mapping logic to Controller

--- src/main/java/org/yarek/fasttestapp/routing/Controller.java
+++ src/main/java/org/yarek/fasttestapp/routing/Controller.java
@@ -5,14 +5,39 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.yarek.fasttestapp.routing.handlers.HttpHandler;
+import org.yarek.fasttestapp.routing.handlers.impl.HomeHandler;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/*")
 public class Controller extends HttpServlet {
 
+    List<HttpHandler> handlers;
+
+    @Override
+    public void init() throws ServletException {
+        registerHandlers();
+    }
+
+    protected void registerHandlers() {
+        handlers = new ArrayList<>();
+        HttpHandler httpHandler;
+        // Home handler
+        httpHandler = new HomeHandler();
+    }
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        String uri = req.getRequestURI();
+        for (HttpHandler handler : handlers) {
+            if (handler.isProcessingPath(uri)) {
+                handler.handle(req, resp);
+                break;
+            }
+        }
     }
+
 }
