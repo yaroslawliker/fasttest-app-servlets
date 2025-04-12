3f3d4ff5b1baf9cd772a1673cc4fac8e5fb0be9d
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-04-12 18:52:44 +0300

    Added: registerQuizPassed Model method

--- src/main/java/org/yarek/fasttestapp/model/Model.java
+++ src/main/java/org/yarek/fasttestapp/model/Model.java
@@ -8,18 +8,20 @@ import org.yarek.fasttestapp.model.exceptions.UsernameAlreadyExistsException;
 import java.util.List;
 
 public interface Model {
-    void registerUser(User user);
+    void registerUser(User user) throws UsernameAlreadyExistsException;
 
     /**
      * Checks if the given password matches given username
      * @return user id if authentication is correct, null otherwise
      */
     String authenticate(String username, String password);
-    User getUser(String userId) throws UsernameAlreadyExistsException;
+    User getUser(String userId);
 
     void getTestPreviews(List<QuizPreview> quizPreviews);
     void getTestPreviews(List<QuizPreview> quizPreviews, int amount);
 
     Quiz getTestById(String testId);
     void saveNewTest(Quiz quiz);
+
+    void registerQuizPassed(String username, String quizId, float score);
 }
