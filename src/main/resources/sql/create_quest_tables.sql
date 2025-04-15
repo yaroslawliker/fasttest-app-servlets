b93c6a8c331c1bc9a2d14a7fd124364912fefaaf
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-04-15 15:49:11 +0300

    Added: create_quest_tables.sql

---
+++ src/main/resources/sql/create_quest_tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE quizzes (
+    id serial PRIMARY KEY,
+    name varchar(100),
+    description text,
+    creation_date date,
+    owner integer REFERENCES users(id)
+);
+
+CREATE TABLE questions (
+    id serial PRIMARY KEY,
+    content text,
+    score integer,
+    quiz integer REFERENCES quizzes(id)
+);
+
+CREATE TABLE answers (
+    id serial PRIMARY KEY,
+    content text,
+    is_correct bool,
+    question integer REFERENCES questions(id)
+);
