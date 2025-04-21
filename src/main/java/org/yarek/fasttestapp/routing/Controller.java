31871e9c2ed3d91f7fa77e014f205b0abddf8b45
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-04-21 13:18:11 +0300

    Added: faster start up

--- src/main/java/org/yarek/fasttestapp/routing/Controller.java
+++ src/main/java/org/yarek/fasttestapp/routing/Controller.java
@@ -24,7 +24,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@WebServlet("/*")
+@WebServlet(value = "/*", loadOnStartup = 1)
 public class Controller extends HttpServlet {
 
     private HikariDataSource dataSource;
