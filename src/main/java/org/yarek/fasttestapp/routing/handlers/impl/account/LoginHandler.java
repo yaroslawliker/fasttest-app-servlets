232ab0812a4e865d4f1fa30366697eb93eb48a57
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-04-27 19:15:32 +0300

    Added: logging to Login and Logout

--- src/main/java/org/yarek/fasttestapp/routing/handlers/impl/account/LoginHandler.java
+++ src/main/java/org/yarek/fasttestapp/routing/handlers/impl/account/LoginHandler.java
@@ -4,6 +4,8 @@ package org.yarek.fasttestapp.routing.handlers.impl.account;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.yarek.fasttestapp.model.database.dao.UserDAO;
 import org.yarek.fasttestapp.model.entities.User;
 import org.yarek.fasttestapp.routing.handlers.HttpHandlerBase;
@@ -14,8 +16,11 @@ import java.util.Objects;
 public class LoginHandler extends HttpHandlerBase {
 
     UserDAO userDAO;
+    Logger logger;
+
 
     public LoginHandler(UserDAO userDAO) {
+        logger = LoggerFactory.getLogger(LoginHandler.class);
         this.userDAO = userDAO;
         addPath("/login");
     }
@@ -27,20 +32,25 @@ public class LoginHandler extends HttpHandlerBase {
 
     @Override
     protected String doPost(HttpServletRequest req, HttpServletResponse resp, Map<String, Object> model) {
+
+
         String username = req.getParameter("username");
         String password = req.getParameter("password");
 
         if (username == null || password == null) {
+            logger.warn("Wrong post request: no username or password params provided.");
             return "wrong_req";
         }
 
         User user = userDAO.getUser(username);
         if (user == null || !Objects.equals(password, user.getPassword())) {
+            logger.info("Attempting login '{}': failed.", username);
             req.setAttribute("notAuthenticatedError", true);
             return "login";
         }
             HttpSession session = req.getSession();
             session.setAttribute("user", user);
+            logger.info("Attempting login '{}': success.", username);
             return "home@redirect";
     }
 }
--- src/main/java/org/yarek/fasttestapp/routing/handlers/impl/account/LogoutHandler.java
+++ src/main/java/org/yarek/fasttestapp/routing/handlers/impl/account/LogoutHandler.java
@@ -3,12 +3,16 @@ package org.yarek.fasttestapp.routing.handlers.impl.account;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yarek.fasttestapp.model.entities.User;
 import org.yarek.fasttestapp.routing.handlers.HttpHandlerBase;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class LogoutHandler extends HttpHandlerBase {
+    Logger logger = LoggerFactory.getLogger(LogoutHandler.class);
 
     public LogoutHandler() {
         addPath("/logout");
@@ -17,6 +21,8 @@ public class LogoutHandler extends HttpHandlerBase {
 
     @Override
     protected String doGet(HttpServletRequest req, HttpServletResponse resp, Map<String, Object> model) throws ServletException, IOException {
+        logger.info("Logged out '{}'", ((User) req.getSession().getAttribute("user")).getUsername());
+
         req.getSession().invalidate();
         return "home@redirect";
     }
